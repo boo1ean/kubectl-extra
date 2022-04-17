25acd84c2990805de424ee827df36ee54e9c7cdc
boo1ean <boo1ean0807@gmail.com>
2022-04-18 00:20:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# kubectl-extra
+
+Works exactly like `kubectl` but with some extra utils
+
+## Installation
+
+```
+npm i -g kubectl-extra
+```
+
+## Command
+
+### Get pod logs
+```
+kubectl-extra p l <query>
+
+# translates into
+
+kubectl logs some-long-pod-name
+```
+
---
+++ src/kubectl-extra.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+const { createProxy } = require('cli-util-proxy')
+const fuzzy = require('fuzzysearch')
+const fuzzySort = require('fuzzysort')
+const { execSync } = require('child_process')
+const cliSelect = require('cli-select')
+
+createProxy('kubectl')
+	.proxy('p l <query>', async (args, done) => {
+		const result = execSync('kubectl get pods')
+		const matches = result
+			.toString()
+			.split('\n')
+			.slice(1)
+			.filter(p => fuzzy(args.params.query, getPodCleanNameFromLine(p)))
+			.map(getPodNameFromLine)
+
+		let match = matches[0]
+
+		if (matches.length > 1) {
+			const sortedMatches = fuzzySort.go(
+				args.params.query,
+				matches
+			).map(r => r.target)
+
+			const { value } = await cliSelect({
+				values: sortedMatches
+			})
+
+			match = value
+		}
+
+		if (!match) {
+			console.log('No matching service.')
+			return done()
+		}
+
+		return `logs ${match}`
+	})
+	.appendOptions()
+	.run()
+
+
+// Remove generated suffixes and concat without separators
+function getPodCleanNameFromLine (p) {
+	const nameTokens = p.split(' ')[0].split('-')
+	return nameTokens.slice(0, -2).join('')
+}
+
+function getPodNameFromLine (p) {
+	return p.split(' ')[0]
+}
